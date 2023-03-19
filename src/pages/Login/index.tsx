70a6dd873be016b8031be3a990e879ab5504857e
Jinsung <46344256+stsoul47@users.noreply.github.com>
2023-03-19 17:56:05 +0900

    [feat] login wrapper 작업 중
    [feat] login wrapper 작업 중

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -9,14 +9,16 @@ const Login = () => {
 			</div>
 			<div className="login__wrapper">
 				<i className="login__wrapper--logo"></i>
-				<div className="button__wrapper">
-					<button>카카오 로그인/회원가입</button>
-					<button>네이버 로그인/회원가입</button>
-					<button>구글 로그인/회원가입</button>
-				</div>
-				<div className="login__bottom">
-					<input type="checkbox" id='checkbox' />
-					<label htmlFor="checkbox"><p>서비스 이용약관</p> 및 <p>개인정보취급방침</p>에 동의하며, 만 14세 이상입니다.</label>
+				<div className="login__content">
+					<div className="button__wrapper">
+						<button>카카오 로그인/회원가입</button>
+						<button>네이버 로그인/회원가입</button>
+						<button>구글 로그인/회원가입</button>
+					</div>
+					<div className="login__bottom">
+						<input type="checkbox" id='checkbox' />
+						<label htmlFor="checkbox"><p>서비스 이용약관</p>&nbsp;및&nbsp;<p>개인정보취급방침</p>에 동의하며, 만 14세 이상입니다.</label>
+					</div>
 				</div>
 			</div>
 		</div>
