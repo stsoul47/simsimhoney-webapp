cb599d30f2b47a7bc6eec923973c1eaa5de2aee5
주현 <96381728+2ee000@users.noreply.github.com>
2023-05-17 17:09:02 +0900

    Feature/sim u 0003 layout (#7)
    * feat: add header
    
    * fix: update header, add comment
    
    * feat: add home page
    
    * fix: update home page

--- src/components/layout/NewnessHeader.tsx
+++ src/components/layout/NewnessHeader.tsx
@@ -8,13 +8,15 @@ import '@styles/components/layout/Header.scss'
  * @since 2023.05.16
  */
 const NewnessHeader = () => {
+	// 페이지 이동 추후 추가(이전 페이지)
+	// 로그인 시 스토리지or쿠키 확인 후 로그인이 되었다면 header--button__login 대신 profile image로 변경, login page로 이동
 	return (
 		<div>
 			<div className='header home-header'>
-				<div className='header--button__back'> // 페이지 이동 추후 추가(이전 페이지)
+				<div className='header--button__back'>
 					<ChevronLeft/>
 				</div>
-				<div className='header--button__login'>Login</div>	// 로그인 시 스토리지or쿠키 확인 후 로그인이 되었다면 header--button__login 대신 profile image로 변경, login page로 이동
+				<div className='header--button__login'>Login</div>
 			</div>			
 		</div>
 	);
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,35 @@
+import NextButton from '@components/button/NextButton';
+import { ReactComponent as Search } from '@assets/Icons/search.svg';
+import RecommendItem from '@components/item/RecommendItem';
+import '@styles/pages/home/home.scss';
+/**
+ * Home Page
+ * 
+ * @author 2ee000
+ * @since 2023.05.17
+ */
 const Home = () => {
 	return (
-		<div>
-			Home 페이지 입니다.
+		<div className='home__wrapper'>
+			<div className='home__content'>
+				<div className='locationSearch'>
+					<p className='locationSearch__title'>새로움을 경험할 장소는 어디인가요?</p>
+					<div className='locationSearch__input'>
+						<input className='locationSearch__input--input' type='text'/>
+						<div className='locationSearch__input--icon'><Search/></div>
+					</div>
+					<a className='locationSearch__link'>내 위치 검색</a>
+				</div>
+				<div className='recommend'>
+					<p className='recommend__title'>여기는 어떠세요?</p>
+					<div className='recommend__wrapper'>
+						<RecommendItem label='강원도 철원 (추천)'/>
+						<RecommendItem label='강원도 속초 (추천)'/>
+						<RecommendItem label='강원도 양양 (추천)'/>
+					</div>
+				</div>
+			</div>
+			<NextButton/>
 		</div>
 	);
 };
